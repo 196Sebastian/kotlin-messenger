3a23b87b1cccab134cc46f3f30dbfcd7e13392b7
Sebastian Correa <correa.sebas6@gmail.com>
2021-12-06 18:41:34 -0500

    Added a register button

--- app/src/main/java/com/example/kotlinmessenger/MainActivity.kt
+++ app/src/main/java/com/example/kotlinmessenger/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.kotlinmessenger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonRegister: Button = findViewById(R.id.button_register)
+
+        buttonRegister.setOnClickListener {
+            val email: EditText = findViewById(R.id.editText_email)
+            val password: EditText = findViewById(R.id.editText_password)
+
+            email.text.toString()
+
+            Log.d("MainActivity", "Email is: $email")
+            Log.d("MainActivity", "Password: $password")
+        }
+
     }
 }
