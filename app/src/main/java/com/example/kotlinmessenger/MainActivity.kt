979d9e16a43e0acc895f1ec2558f3558c01dad7b
Sebastian Correa <correa.sebas6@gmail.com>
2021-12-07 22:02:25 -0500

    Finished creating register and login UI

--- app/src/main/java/com/example/kotlinmessenger/MainActivity.kt
+++ app/src/main/java/com/example/kotlinmessenger/MainActivity.kt
@@ -5,23 +5,30 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val buttonRegister: Button = findViewById(R.id.button_register)
-
-        buttonRegister.setOnClickListener {
-            val email: EditText = findViewById(R.id.editText_email)
-            val password: EditText = findViewById(R.id.editText_password)
-
-            email.text.toString()
+        findViewById<Button>(R.id.button_register).setOnClickListener {
+            val email: String = findViewById<EditText>(R.id.editText_email).text.toString()
+            val password: String = findViewById<EditText>(R.id.editText_password).text.toString()
 
             Log.d("MainActivity", "Email is: $email")
             Log.d("MainActivity", "Password: $password")
         }
+
+        findViewById<TextView>(R.id.already_have_account_textView).setOnClickListener{
+            Log.d("MainActivity", "Try to show login activity")
+
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+
 
     }
 }
