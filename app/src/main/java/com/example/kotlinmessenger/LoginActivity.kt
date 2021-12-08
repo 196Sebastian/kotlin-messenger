75b9cf183e8944c487998fb3e155881acd191771
Sebastian Correa <correa.sebas6@gmail.com>
2021-12-08 08:40:40 -0500

    Updated login UI & made button back_to_registration functional

--- app/src/main/java/com/example/kotlinmessenger/LoginActivity.kt
+++ app/src/main/java/com/example/kotlinmessenger/LoginActivity.kt
@@ -1,11 +1,29 @@
 package com.example.kotlinmessenger
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_login)
+
+        findViewById<Button>(R.id.button_login).setOnClickListener {
+            val email: String = findViewById<EditText>(R.id.editText_login_email).text.toString()
+            val password: String = findViewById<EditText>(R.id.editText_login_password).text.toString()
+
+            Log.d("Login", "Attempt login with email/password: $email/***")
+        }
+
+        findViewById<TextView>(R.id.back_to_registration).setOnClickListener{
+            finish()
+        }
+
+
     }
 }
